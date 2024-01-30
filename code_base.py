3ca2640b04904ed99c33499b1d1866fa6a19304c
Varioucha <varvara.wyart@eduvaud.ch>
2024-01-30 08:37:41 +0100

    chager input

--- code_base.py
+++ code_base.py
@@ -1,11 +1,11 @@
 # changer pour que ca soit ordi qui génère les mots avec la list
-mot_choisi = input("hello")
+mot_choisi = input("mot a faire deviner")
 
 nombre_de_erreur = 0
 
 #tant que le nombre d'erreur autorisé n'est pas dépacé le jeux continue
 while nombre_de_erreur < 7:
-    lettre_essaie = input("fgd")
+    lettre_essaie = input("essayez une lettre")
     if lettre_essaie in mot_choisi:
         print("Bravo!")
         #supp lettre de la list
