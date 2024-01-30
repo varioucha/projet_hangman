e1f9a95da58f12a1d42442c2655cb06c0c58f518
roxanakho <pa38rbi@eduvaud.ch>
2024-01-30 08:33:24 +0100

    code updated

--- code_base.py
+++ code_base.py
@@ -1,13 +1,12 @@
 # changer pour que ca soit ordi qui génère les mots avec la list
-mot_choisi = input()
+mot_choisi = input("hello")
 
-list_lettres_du_mot_choisi = mot_choisi.split()
 nombre_de_erreur = 0
 
 #tant que le nombre d'erreur autorisé n'est pas dépacé le jeux continue
 while nombre_de_erreur < 7:
-    lettre_essaie = input()
-    if lettre_essaie in list_lettres_du_mot_choisi:
+    lettre_essaie = input("fgd")
+    if lettre_essaie in mot_choisi:
         print("Bravo!")
         #supp lettre de la list
     else:
