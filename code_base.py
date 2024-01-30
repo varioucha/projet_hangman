1c3694e108467fc824cfc955c04205a78fe91d48
roxanakho <pa38rbi@eduvaud.ch>
2024-01-30 08:55:25 +0100

    alphabet ajouté

--- code_base.py
+++ code_base.py
@@ -3,6 +3,7 @@ mot_choisi = input("mot a faire deviner")
 
 nombre_de_erreur = 0
 
+alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 #tant que le nombre d'erreur autorisé n'est pas dépacé le jeux continue
 while nombre_de_erreur < 7:
     lettre_essaie = input("essayez une lettre")
