f9c4a6bf64b478b9e84b1e63cf55b887fbc4a41f
Varioucha <varvara.wyart@eduvaud.ch>
2024-02-20 09:17:05 +0100

    changement

--- codebase.py
+++ codebase.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+import random
+
 def display_word_state(mot_a_deviner, lettres_essai):
     # Initialiser le mot affiché comme une liste de tirets ou de lettres
     displayed_word = [letter if letter in lettres_essai else '_' for letter in mot_a_deviner]
@@ -9,26 +12,56 @@ def display_word_state(mot_a_deviner, lettres_essai):
     remaining_letters = len([letter for letter in mot_a_deviner if letter not in lettres_essai])
     print(f"Il reste {remaining_letters} lettre(s) à trouver.")
 
-# Définir un mot à deviner
-#cette partie va prendre un mot du fichier aleatoirement et le met en majuscule
-import random
-fichier = open("list_mots_2.txt", "r")
-list_de_mots = fichier.readlines()
+# Message de bienvenue
+print("Bienvenue dans le jeu du pendu")
+print("-------------------------------------------")
+
+#choisir le niveau (list de mots un ou deux)
+niveau = input("choissisez votre niveau: (1) : mots facile, (2) : mots difficile.")
+if niveau == 1:
+    print("niveau choisi facile")
+    # Obtenir le chemin complet vers le script courant x2
+    script_path = Path(__file__).resolve()
+
+    #récupérer le chemin vers le dossier parent du script courant
+    script_dir = script_path.parent
+
+    #On ajoute le nom de notre fichier
+    path = str(script_dir)+ "/list_mots_2.txt"
+
+    #on ouvre le fichier file.txt
+    f = open(path, "r")
+
+else:
+    print("niveau choisi difficile")
+    #pour niveau diff
+    script_path = Path(__file__).resolve()
+
+    #récupérer le chemin vers le dossier parent du script courant
+    script_dir = script_path.parent
+
+    #On ajoute le nom de notre fichier
+    path = str(script_dir)+ "/list_mots_diff_2.txt"
+
+    #on ouvre le fichier file.txt
+    f = open(path, "r")
+
+
+#definir mot
+list_de_mots = f.readlines()
 
 mot_a_deviner = random.choice(list_de_mots)
 mot_a_deviner = mot_a_deviner.upper()
 
 lettres_essai = []
-# Message de bienvenue
-print("Bienvenue dans le jeu du pendu")
-print("-------------------------------------------")
-
 # Nombre d'erreurs et Alphabet
 nombre_de_erreurs = 0
 alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 
+display_word_state(mot_a_deviner, lettres_essai)
+
 # Boucle de jeu
-while nombre_de_erreurs < 7 and set(mot_a_deviner) != set(lettres_essai):
+while nombre_de_erreurs < 6 and set(mot_a_deviner) != set(lettres_essai):
     lettre_essai = input("Essayez une lettre: ").upper()
 
     if lettre_essai in mot_a_deviner:
@@ -48,7 +81,7 @@ while nombre_de_erreurs < 7 and set(mot_a_deviner) != set(lettres_essai):
     if set(mot_a_deviner) == set(lettres_essai):
         print("Félicitations ! Vous avez trouvé le mot.")
         break
-    elif nombre_de_erreurs >= 7:
+    elif nombre_de_erreurs >= 6:
         print("Jeu terminé. Vous avez dépassé le nombre d'erreurs autorisé.")
         break
 
