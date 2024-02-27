693a0f8a75f09509af197cf84865c157ada6b9eb
Varioucha <varvara.wyart@eduvaud.ch>
2024-02-27 08:57:45 +0100

    tk

--- tk_Roxana.py
+++ tk_Roxana.py
@@ -1,10 +1,17 @@
 from tkinter import *
 
+
+#creation de la fenetre
 window = Tk()
 window.title("Hangman")
 window.geometry("300x200")
 
+#text
+label_word = Label(window, text= "Hello",  bg="white", fg="black")
+label_word.place(x=100, y=200)
 
-label = Label(window, text= "Hello",  bg="white", fg="black")
-label.place(x=100, y=200)
+#image
+image_1 = PhotoImage(file="image.png")
+label = Label(image=image_1)
+
 window.mainloop()
